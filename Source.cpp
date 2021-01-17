64d30c258d187e3b1c2ec0a8ec9f4e389f362a74
mituionutz <75694105+mituionutz@users.noreply.github.com>
2021-01-17 18:10:50 +0000

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <string>
 #include"film.h"
+#include"rezervare.h"
+#include"sala.h"
+#include"client.h"
+#include"card.h"
 using namespace std;
 
 
@@ -8,12 +12,13 @@ using namespace std;
 
 int main()
 {
-	//int rating[3] = {10,9,8};
+    cout << "----------- CLASA FILM -----------" << endl;
+	int rating[] = {10};
 	//Film f1;
 	//Film f2 = f1;
  //   Film f3(rating, "Greenland", "VIP", 17, 12.15, 90);
-	//Film f4(rating, "Creed", "Terra", 33, 25.16, 105);
- //   cout << f3 << f4;
+	Film f4( "Creed", "Terra", 1, rating, 25.16, 105);
+    cout <<f4;
 	//f1.setSala("Gold");
 	//cout << f1.getSala() << endl;
 	//cout << f3.getSpectatori() << endl;
@@ -24,5 +29,13 @@ int main()
 	////cout << f1 << f2 << f3;
 	//Film f5 = f1;
 	//cout << endl;
+	cout << "----------- CLASA CLIENT -----------" << endl;
+	int filmeVazute[] = { 7 };
+	Client c1;
+	Client c2 = c1;
+	Client c3(4,"ionut","no mail",1, filmeVazute, 1952);
+	Client c4(c3);
+	cout << c3 << c4;
+	
 	
 }
